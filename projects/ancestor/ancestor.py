028773f5d7a5da80f98e5da5f353acb47a430672
foobar404 <austinthemichaud@gmail.com>
2020-03-17 13:16:43 -0700

    finish ancestors assignment

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,55 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
+def makeRelationships(family):
+    tree = {}
+
+    for people in family:
+        if people[1] in tree:
+            tree[people[1]].append(people[0])
+        else:
+            tree[people[1]] = [people[0]]
+
+    return tree
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    relationships = makeRelationships(ancestors)
+    stack = Stack()
+    oldest = {"length": 1, "node": starting_node}
+    stack.push(oldest)
+
+    if starting_node not in relationships:
+        return -1
+
+    while stack.size() > 0:
+        node = stack.pop()
+        if node["length"] >= oldest["length"]:
+            if node["length"] == oldest["length"]:
+                if node["node"] < oldest["node"]:
+                    oldest = node
+            else:
+                oldest = node
+
+        if node["node"] not in relationships:
+            continue
+        for parent in relationships[node["node"]]:
+            new_node = {"length": node["length"] + 1, "node": parent}
+            stack.push(new_node)
+
+    return oldest["node"]
+
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,20 +1,34 @@
+"""
+Simple graph implementation
+"""
 from util import Stack, Queue  # These may come in handy
 
 
 class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
         self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
         else:
             raise ValueError("vertex does not exist")
 
     def add_undirected_edge(self, v1, v2):
+        """
+        Add an undirected edge to the graph.
+        """
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
             self.vertices[v2].add(v1)
@@ -22,12 +36,19 @@ class Graph:
             raise ValueError("vertex does not exist")
 
     def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
         if vertex_id in self.vertices:
             return self.vertices[vertex_id]
         else:
             raise ValueError("vertex does not exist")
 
     def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
         # Create a queue
         q = Queue()
         # Enqueue the starting vertex
@@ -49,6 +70,10 @@ class Graph:
                     q.enqueue(neighbor)
 
     def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
         # Create a stack
         s = Stack()
         # Push the starting vertex
@@ -69,50 +94,159 @@ class Graph:
                 for neighbor in self.get_neighbors(v):
                     s.push(neighbor)
 
-    #!MINE
-    def dft_recursive(self, vertex, stack=None, history={}):
-        if stack == None:
-            stack = Stack()
-        else:
-            if vertex in history:
-                return
-
-            if stack.size == 0:
-                return
-
-        print(vertex)
-        history[vertex] = True
-
-        for neighbor in self.vertices[vertex]:
-            if neighbor not in history:
-                stack.push(neighbor)
-
-        self.dft_recursive(stack.pop(), stack, history)
+    def dft_recursive(self, starting_vertex, visited=None):
+        if visited is None:
+            visited = set()
+        # Check if the node has been visited
+        # If not...
+        if starting_vertex not in visited:
+            # Mark it as visited
+            visited.add(starting_vertex)
+            print(starting_vertex)
+            # Call dft_recursive on each neighbor
+            for neighbor in self.get_neighbors(starting_vertex):
+                self.dft_recursive(neighbor, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
-        que = Queue()
-        trigger = True
-        node = starting_vertex
-        history = {}
+        # Create a queue
+        q = Queue()
+        # Enqueue A PATH TO the starting vertex
+        q.enqueue([starting_vertex])
+        # Create a set to store visited vertices
+        visited = set()
+        # While the queue is not empty...
+        while q.size() > 0:
+            # Dequeue the first PATH
+            path = q.dequeue()
+            # GRAB THE VERTEX FROM THE END OF THE PATH
+            v = path[-1]
+            # Check if it's been visited
+            # If it hasn't been visited...
+            if v not in visited:
+                # Mark it as visited
+                visited.add(v)
+                # CHECK IF IT'S THE TARGET
+                if v == destination_vertex:
+                    # IF SO, RETURN THE PATH
+                    return path
+                # Enqueue A PATH TO all it's neighbors
+                for neighbor in self.get_neighbors(v):
+                    # MAKE A COPY OF THE PATH
+                    path_copy = path.copy()
+                    path_copy.append(neighbor)
+                    # ENQUEUE THE COPY
+                    q.enqueue(path_copy)
 
-        while trigger:
-            for neighbor in self.vertices[node]:
-                que.add(node)
+    def dfs(self, starting_vertex, destination_vertex):
+        # Create a stack
+        s = Stack()
+        # Push A PATH TO the starting vertex
+        s.push([starting_vertex])
+        # Create a set to store visited vertices
+        visited = set()
+        # While the stack is not empty...
+        while s.size() > 0:
+            # Pop the first PATH
+            path = s.pop()
+            # GRAB THE VERTEX FROM THE END OF THE PATH
+            v = path[-1]
+            # Check if it's been visited
+            # If it hasn't been visited...
+            if v not in visited:
+                # Mark it as visited
+                visited.add(v)
+                # CHECK IF IT'S THE TARGET
+                if v == destination_vertex:
+                    # IF SO, RETURN THE PATH
+                    return path
+                # Enqueue A PATH TO all it's neighbors
+                for neighbor in self.get_neighbors(v):
+                    # MAKE A COPY OF THE PATH
+                    path_copy = path.copy()
+                    path_copy.append(neighbor)
+                    # PUSH THE COPY
+                    s.push(path_copy)
 
-            node = que.dequeue()
-            history[node] = True
-
-            if node == destination_vertex:
-                return
-
-            if que.size == 0:
-                trigger = False
-
+    def dfs_recursive(
+        self, starting_vertex, destination_vertex, visited=None, path=None
+    ):
+        if visited is None:
+            visited = set()
+        if path is None:
+            path = []
+        if starting_vertex not in visited:
+            visited.add(starting_vertex)
+            path_copy = path.copy()
+            path_copy.append(starting_vertex)
+            if starting_vertex == destination_vertex:
+                return path_copy
+            for neighbor in self.get_neighbors(starting_vertex):
+                new_path = self.dfs_recursive(
+                    neighbor, destination_vertex, visited, path_copy
+                )
+                if new_path is not None:
+                    return new_path
         return None
 
-    def dfs():
-        pass
 
-    def dfs_recursive():
-        pass
-
+if __name__ == "__main__":
+    graph = Graph()
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
+    """
+    Should print:
+        {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+    """
+    print(graph.vertices)
+    """
+    Valid BFT paths:
+        1, 2, 3, 4, 5, 6, 7
+        1, 2, 3, 4, 5, 7, 6
+        1, 2, 3, 4, 6, 7, 5
+        1, 2, 3, 4, 6, 5, 7
+        1, 2, 3, 4, 7, 6, 5
+        1, 2, 3, 4, 7, 5, 6
+        1, 2, 4, 3, 5, 6, 7
+        1, 2, 4, 3, 5, 7, 6
+        1, 2, 4, 3, 6, 7, 5
+        1, 2, 4, 3, 6, 5, 7
+        1, 2, 4, 3, 7, 6, 5
+        1, 2, 4, 3, 7, 5, 6
+    """
+    # graph.bft(1)
+    # '''
+    # Valid DFT paths:
+    #     1, 2, 3, 5, 4, 6, 7
+    #     1, 2, 3, 5, 4, 7, 6
+    #     1, 2, 4, 7, 6, 3, 5
+    #     1, 2, 4, 6, 3, 5, 7
+    # '''
+    # graph.dft(1)
+    # graph.dft_recursive(1)
+    # '''
+    # Valid BFS path:
+    #     [1, 2, 4, 6]
+    # '''
+    # print(graph.bfs(1, 6))
+    # '''
+    # Valid DFS paths:
+    #     [1, 2, 4, 6]
+    #     [1, 2, 4, 7, 6]
+    # '''
+    # print(graph.dfs(1, 6))
+    print(graph.dfs_recursive(1, 6))
